1344e89e695344f6cf7c1ef44554f8f65671c9bf
buglas <liwei1051904257@gmail.com>
2022-06-16 22:41:33 +0800

    Update Cabinet.ts

--- 02-machineroom/src/server/Cabinet.ts
+++ 02-machineroom/src/server/Cabinet.ts
@@ -1,4 +1,5 @@
-const path = "http://127.0.0.1:4523/mock/772752/cabinet/";
+// const path = "http://127.0.0.1:4523/mock/772752/cabinet/";
+const path = '	https://mock.apifox.cn/m1/772752-0-default/cabinet/'
 function getCabinetByName(name: string) {
   return fetch(path + name).then((res) => res.json());
 }
